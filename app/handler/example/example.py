623b49c4cf70a87188628fbecd65a0318a9920c8
caiqingjing <1125862926@qq.com>
2020-07-15 17:02:01 +0800

    feat(config_way):1.添加配置文件配置方法 2. 加上 autoreload

--- app/handler/example/example.py
+++ app/handler/example/example.py
@@ -3,9 +3,9 @@ from app.route import BaseApplication as router
 from app.handler.error import BadRequestError
 
 
-@router.route('/api/v1/example/get')
+@router.route("/api/v1/example/get")
 class ExampleHandler(BaseHandler):
 
     def get(self):
-        # raise BadRequestError(reason='什么什么玩意儿')
-        return self.write_json(data='Hello world')
+        # raise BadRequestError(reason="什么什么玩意儿")
+        return self.write_json(data="Hello world")
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 """
-Tornado Web 应用程序通常组成：一个或多个 RequestHandler 子类，一个 Application 对象
-会将所有的 request 都路由到对应的 Handler 上。那么为什么还存在多个 Applications 对应一
-个 Server 的情况，两种结构各自的优缺点又是什么。
+    Tornado Web 应用程序通常组成：一个或多个 RequestHandler 子类，一个 Application 对象
+    会将所有的 request 都路由到对应的 Handler 上。那么为什么还存在多个 Applications 对应一
+    个 Server 的情况，两种结构各自的优缺点又是什么。
 """
 from tornado.ioloop import IOLoop
 from tornado.web import HTTPServer
+from tornado.options import options, define
+from tornado import autoreload
 
 from app.route import BaseApplication
 from app.handler.example.example import ExampleHandler
-
+from settings import *
 
 router = BaseApplication()
 
+
 if __name__ == "__main__":
+
+    if options.autoreload: autoreload.start()
+
     server = HTTPServer(router)
-    server.bind(8009)
-    server.start(0)
+    server.listen(options.port)
     IOLoop.current().start()
