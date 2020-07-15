d728d5fb0861e07e2e87573fc512eab853be489f
caiqingjing <1125862926@qq.com>
2020-07-15 14:12:25 +0800

    fix(route_mapping): 修复路由映射错误

--- app/handler/example/example.py
+++ app/handler/example/example.py
@@ -1,15 +1,8 @@
 from app.handler import BaseHandler
-<<<<<<< HEAD
-from app.route import BaseApplication as app
-
-
-@app.route('/api/v1/example/get')
-=======
-from app.route import BaseRouter as router
+from app.route import BaseApplication as router
 
 
 @router.route('/api/v1/example/get')
->>>>>>> dev
 class ExampleHandler(BaseHandler):
 
     def get(self):
